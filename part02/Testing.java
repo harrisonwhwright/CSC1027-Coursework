ae7d70d07cab593c1b39779d3f0bd3bc2cf26481
harrisonwhwright <121120664+harrisonwhwright@users.noreply.github.com>
2024-12-03 15:34:54 +0000

    Add files via upload

--- part02/Testing.java
+++ part02/Testing.java
@@ -60,7 +60,7 @@ public class Testing {
         test ("2.1.5",
     			"Adding Artifact with Null Type",
     			"\n     name = Acropolis Statues\n     type = null\n     engagementTime = 5",
-    			"Error Message, Don't Add");
+    			"Success, Change Type to OTHER");
         try {
         	QUBMuseum.addArtifacts("Acropolis Statues", null, 10);
         	} catch (Exception exception) {
@@ -71,7 +71,7 @@ public class Testing {
         test ("2.1.6",
     			"Adding Artifact with Invalid Type",
     			"\n     name = Acropolis Statues\n     type = 12345\n     engagementTime = 5",
-    			"Error Message, Don't Add");
+    			"Success, Change Type to OTHER");
         try {
         	QUBMuseum.addArtifacts("Acropolis Statues", 12345, 10);
         	} catch (Exception exception) {
@@ -80,8 +80,236 @@ public class Testing {
         endTest();       
                 
                 
-                
-                
+        test ("2.1.7",
+    			"Adding Artifact with Null Engagement Time",
+    			"\n     name = Acropolis Statues\n     type = Sculpture\n     engagementTime = null",
+    			"Error Message, Don't Add");
+        try {
+        	QUBMuseum.addArtifacts("Acropolis Statues", Type.SCULPTURE, null);
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();  
+
+        test ("2.1.8",
+    			"Adding Artifact with Invalid Engagement Time Data Type",
+    			"\n     name = Acropolis Statues\n     type = Sculpture\n     engagementTime = abcde",
+    			"Error Message, Don't Add");
+        try {
+        	QUBMuseum.addArtifacts("Acropolis Statues", Type.SCULPTURE, "abcde");
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();  
+        
+        test ("2.2.1",
+    			"Viewing Artifact with Null ID",
+    			"\n     iD = null",
+    			"Error Message, Display that there is nothing to view");
+        try {
+        	QUBMuseum.viewArtifactById(null);
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();  
+        
+        test ("2.2.2",
+    			"Viewing Artifact with Valid ID",
+    			"\n     iD = 1",
+    			"Success");
+        try {
+        	QUBMuseum.viewArtifactById(1);
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();  
+        
+        test ("2.2.3",
+    			"Viewing Artifact with Invalid ID Data Type",
+    			"\n     iD = abcde",
+    			"Error Message, Display that there is nothing to view");
+        try {
+        	QUBMuseum.viewArtifactById("abcde");
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        test ("2.2.4",
+    			"Viewing Artifact that does not Exist",
+    			"\n     iD = 999",
+    			"Error Message, Display that there is nothing to view");
+        try {
+        	QUBMuseum.viewArtifactById(999);
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        test ("2.2.5",
+    			"Veiwing Artifact with Null Name",
+    			"\n     name = null",
+    			"Error Message, Display that there is nothing to view");
+        try {
+        	QUBMuseum.viewArtifactByName(null);
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        test ("2.2.6",
+    			"Veiwing Artifact with Valid Name",
+    			"\n     name = Acropolis Statues",
+    			"Success");
+        try {
+        	QUBMuseum.viewArtifactByName("Acropolis Statues");
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        test ("2.2.7",
+    			"Veiwing Artifact with Invalid Name Data Type",
+    			"\n     name = 12345",
+    			"Error Message, Display that there is nothing to view");
+        try {
+        	QUBMuseum.viewArtifactByName(12345);
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        test ("2.2.8",
+    			"Veiwing Artifact with Null Part Name",
+    			"\n     partName = null",
+    			"Error Message, Display that there is nothing to view");
+        try {
+        	QUBMuseum.viewArtifactByPartName(null);
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        test ("2.2.9",
+    			"Veiwing Artifact with Valid Part Name",
+    			"\n     partName = Acropolis",
+    			"Success");
+        try {
+        	QUBMuseum.viewArtifactByPartName("Acropolis");
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        test ("2.2.10",
+    			"Viewing Artifact with Invalid Part Name Data Type",
+    			"\n     partName = 12345",
+    			"Error Message, Display that there is nothing to view");
+        try {
+        	QUBMuseum.viewArtifactByPartName(12345);
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        test ("2.2.11",
+    			"Viewing Artifact with Null Type",
+    			"\n     type = null",
+    			"Error Message, Display that there is nothing to view");
+        try {
+        	QUBMuseum.viewArtifactByType(null);
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        test ("2.2.12",
+    			"Viewing Artifact with Valid Type",
+    			"\n     type = Painting",
+    			"Success");
+        try {
+        	QUBMuseum.viewArtifactByType(Type.PAINTING);
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        test ("2.2.13",
+    			"Viewing Artifact with Invalid Type Data Type",
+    			"\n     type = 12345",
+    			"Error Message, Display that there is nothing to view");
+        try {
+        	QUBMuseum.viewArtifactByType(12345);
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        test ("2.3.1",
+    			"Deleting Artifact that does not Exist",
+    			"\n     id = 999",
+    			"Error Message, Display that there is nothing to delete");
+        try {
+        	QUBMuseum.deleteArtifacts(999);
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        test ("2.3.2",
+    			"Deleting Artifact with null ID",
+    			"\n     id = null",
+    			"Error Message, Display that there is nothing to delete");
+        try {
+        	QUBMuseum.deleteArtifacts(null);
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        test ("2.3.3",
+    			"Deleting Artifact with Valid ID",
+    			"\n     id = 2",
+    			"Success");
+        try {
+        	QUBMuseum.deleteArtifacts(2);
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        test ("2.3.4",
+    			"Deleting Artifact with Invalid ID Data Type",
+    			"\n     id = abcde",
+    			"Error Message, Display that there is nothing to delete");
+        try {
+        	QUBMuseum.deleteArtifacts("abcde");
+        	} catch (Exception exception) {
+        		exception.printStackTrace();
+        	}
+        endTest();
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 
     public static void test (String testCase, String objective, String testData, String expectedResult) {
